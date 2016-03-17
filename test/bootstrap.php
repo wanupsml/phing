c3926ed6a344469dd93bea9779582454c0f526b4
Alex Corretge <alex@corretge.cat>
2016-03-17 12:00:18 +0100

    PA-55 added new argument to Copy: overwriteDates to keep files even if origin is newer than destination

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -3,7 +3,6 @@ defined('PHING_TEST_BASE') || define('PHING_TEST_BASE', dirname(__FILE__));
 set_include_path(
     realpath(dirname(__FILE__) . '/../classes') . PATH_SEPARATOR .
     realpath(dirname(__FILE__) . '/classes') . PATH_SEPARATOR .
-    realpath(dirname(__FILE__) . '/../vendor/pear/http_request2') . PATH_SEPARATOR .
     get_include_path()  // trunk version of phing classes should take precedence
 );
 
